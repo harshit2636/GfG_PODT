e288670253d8dc53c5de1b0ab50cf1d8f3a59f1a
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-03-13 00:32:12 +0530

    Add files via upload

--- POTD.java
+++ POTD.java
@@ -1,34 +1,46 @@
 class Solution {
-    int find(int mat2[][],int n,int x)
-    {
-        int k1=0;
-        int k2=n-1;
-        while(k1<n && k2>=0)
-        {
-            if(mat2[k1][k2]==x)
-                return x;
-            else if(mat2[k1][k2]>x)
-                k2--;
-            else if(mat2[k1][k2]<x)
-                k1++;
-        }
-        return 0;
-    }
-    int countPairs(int mat1[][], int mat2[][], int n, int x) {
-        // code here
-        int c=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(mat1[i][j]<x)
-                {
-                    int t=find(mat2,n,x-mat1[i][j]);
-                    if(t>0)
-                        c++;
+    public int[] matrixDiagonally(int[][] mat) {
+        int eo = 0;//check even or odd.
+        int row = mat.length;
+        int col = mat[0].length;
+        int count = 0;
+        int ans[] = new int[row*col];
+
+        for(int i=0;i<row;i++){
+            if(eo%2!=0){
+                int pos = 0;
+                for(int j=i;j>=0;j--){
+                    ans[count++] = mat[pos][j];
+                    pos++;
+                }
+            }else{
+                int pos = i;
+                for(int j=0;j<=i;j++){
+                    ans[count++] = mat[pos][j];
+                    pos--;
                 }
             }
+            eo++;
         }
-        return c;
+
+        for(int i=1;i<row;i++){
+            if(eo%2!=0){
+                int pos = i;
+                for(int j=col-1;j>=i;j--){
+                    ans[count++] = mat[pos][j];
+                    pos++;
+                }
+            }else{
+                int pos = row-1;
+                for(int j=i;j<col;j++){
+                    ans[count++] = mat[pos][j];
+                    pos--;
+                }
+            }
+            eo++;
+        }
+
+        return ans;
+
     }
 }
