ad76df7d827824f1b32a717d78482ce9780b01da
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-03-15 00:14:36 +0530

    Add files via upload

--- PODT.java
+++ PODT.java
@@ -1,14 +1,24 @@
 class Solution {
-    String removeDuplicates(String str) {
-        Set<Character> set = new HashSet<>();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (!set.contains(ch)) {
-                result.append(ch);
-                set.add(ch);
-            }
+    public Node sort(Node head){
+        if(head==null) return head;
+        Node n=head, q=head;
+        int count=1, i=0;
+        while(n.next!=null){
+            count++;
+            n=n.next;
         }
-        return result.toString();
+        Integer arr[]=new Integer[count];
+        n=head;
+        while(i<count){
+            arr[i]=n.data;
+            n=n.next;
+            i++;
+        }
+        Arrays.sort(arr);
+        for(i=0;i<count;i++){
+            q.data=arr[i];
+            q=q.next;
+        }
+        return head;
     }
 }
