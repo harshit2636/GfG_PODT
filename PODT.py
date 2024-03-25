2a2df0530ef4f6208f44f22b669b58ec85ee9304
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-03-25 17:02:51 +0530

    Add files via upload

--- PODT.py
+++ PODT.py
@@ -1,25 +1,16 @@
-class Solution:
-    def largestSubsquare(self, N, A):
-        ROWS,COLS = len(A),len(A[0])
-        dp = [[[0,0] for i in range(ROWS+1)] for j in range(COLS+1)]
+class Solution:     # line 1
+	def NBitBinary(self, n):
+		res = []
+		
+		def oneorzero(string, length, zeros):
+		    if length == n:
+		        res.append(string)
+		        return
+		    
+		    oneorzero(string+'1', length+1, zeros)
+		    
+		    if (length-zeros) > zeros:
+		        oneorzero(string+'0', length+1, zeros+1)
         
-        for i in range(ROWS):
-            for j in range(COLS):
-                if A[i][j] == 'X':
-                    dp[i+1][j+1][0] = dp[i][j+1][0] + 1
-                    dp[i+1][j+1][1] = dp[i+1][j][1] + 1
-                    
-                    
-        maxi = 0
-        
-        for i in range(ROWS,0,-1):
-            for j in range(COLS,0,-1):
-                curMin = min(dp[i][j][0],dp[i][j][1])
-                while curMin > maxi:
-                    if dp[i-curMin+1][j][1] >= curMin and dp[i][j-curMin+1][0] >= curMin:
-                        maxi = curMin
-                        
-                    else:
-                        curMin -= 1
-                        
-        return maxi
+        oneorzero('1', 1, 0)
+        return res
