6e7e6ca76e292a93b26114358c614c0db24dbca2
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-03-26 22:35:43 +0530

    Add files via upload

--- Podt.py
+++ Podt.py
@@ -1,17 +1,22 @@
-from collections import defaultdict
 class Solution:
-    def diagonalSum(self, root):
-        d=defaultdict(int)
-        
-        def helper(root,slope):
-            if(root is not None):
-                d[slope]+=root.data
-                helper(root.left,slope+1)
-                helper(root.right,slope)
-        
-        helper(root,0)
-        res=[]
-        n=len(d)
+    def isAdditiveSequence(self, st):
+        n=len(st)
         for i in range(n):
-            res.append(d[i])
-        return res
+            for j in range(i+1,n):
+                s,e=st[:i+1],st[i+1:j+1]
+                ind=j+1
+                re=str(int(s)+int(e))
+                le=len(re)
+                # if(ind==2):
+                #     print(s,e,re,st[ind:ind+le])
+                while(re==st[ind:ind+le]):
+                    s,e=e,re
+                    ind+=le
+                    re=str(int(s)+int(e))
+                    le=len(re)
+                    # print(s,e,re,st[ind:ind+le],ind,22222)
+                    if(ind==len(st)):
+                        return(1)
+                    elif(ind>len(st)):
+                        break
+        return(0)
