def1cdcc5d2dfed75d6f4866540f73ef60ffd924
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-04-08 21:07:14 +0530

    Add files via upload

--- POTD.py
+++ POTD.py
@@ -1,6 +1,18 @@
-class Solution:
-    #Function to delete a node without any reference to head pointer.
-    def deleteNode(self,curr_node):
-            #code here
-            curr_node.data=curr_node.next.data
-            curr_node.next=curr_node.next.next
+class solve {
+    static long countMaximum(int n, int arr[]) {
+        int[][] dp = new int[n][n];
+        for (int g = 0; g < n; g++) {
+            for (int i = 0, j = g; j < n; i++, j++) {
+                if (g == 0) {
+                    dp[i][j] = arr[i];
+                } else if (g == 1) {
+                    dp[i][j] = Math.max(arr[i], arr[j]);
+                } else {
+                    dp[i][j] = Math.max(arr[i] + Math.min(dp[i + 2][j], dp[i + 1][j - 1]), 
+                                        arr[j] + Math.min(dp[i + 1][j - 1], dp[i][j - 2]));
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
