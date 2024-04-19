c1cbe8386fcd5215d0cb5943f591b494b81006bc
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-04-19 19:44:47 +0530

    Add files via upload

--- 19-04-24.java
+++ 19-04-24.java
@@ -1,22 +1,17 @@
 class Solution
 {
-    //Function to find two repeated elements.
-    public int[] twoRepeated(int arr[], int n)
+    ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
     {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<n+2;i++){
-            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i<m;i++){
+            set.add(b[i]);
         }
-        int [] ans=new int[2];
-        Arrays.fill(ans,-1);
-        int j=1;
-        for(int i=n+1;i>=0;i--){
-            if(j==-1) break;
-            if(hm.get(arr[i])==2 && ans[1] != arr[i]) {
-                ans[j]=arr[i];
-                j--;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            if(!set.contains(a[i])){
+                ans.add(a[i]);
+            }
         }
-    }
-       return ans;
+        return ans;
     }
 }
--- 19-04-24.py
+++ 19-04-24.py
@@ -1,12 +1,8 @@
 class Solution:
-    def twoRepeated(self, arr , n):
-        temp = {}
-        ans,count = [],0
-        for i in arr:
-            if count==2:
-                break
-            temp[i] = temp.get(i,0)+1
-            if temp[i]==2:
+    def findMissing(self,a,b,n,m):
+        ans=[]
+        k=set(b)
+        for i in a:
+            if i not in k:
                 ans.append(i)
-                count+=1
         return ans
