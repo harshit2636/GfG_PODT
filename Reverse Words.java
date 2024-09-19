74f43155a9758f96ca5152da06fdf58dda7d6e08
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-09-19 21:47:12 +0530

    Create Reverse Words.java

---
+++ Reverse Words.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Function to reverse words in a given string.
+    String reverseWords(String str) {
+        String[] words = str.split("\\.");
+        
+        StringBuilder reversed = new StringBuilder();
+        
+        for(int i=words.length-1; i>=0; i--){
+            reversed.append(words[i]);
+            
+            if(i>0){
+                reversed.append(".");
+            }
+        }
+        
+        return reversed.toString();
+    }
+}
